e2b6a5e5e3925b98d9d5682c54067c0bf05720db
AntonCkya <sinyukovanton@yandex.ru>
2020-05-03 20:18:14 +0500

    UPDATE: как психованный сохраняю ничего почти расставил обьекты в стат активити ( только портрет ) ноут баганул, не видит блюпуп уши, перезагружаем

--- app/src/main/java/com/example/itschoolproject_bullscows/StatisticsActivity.java
+++ app/src/main/java/com/example/itschoolproject_bullscows/StatisticsActivity.java
@@ -3,12 +3,18 @@ package com.example.itschoolproject_bullscows;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class StatisticsActivity extends AppCompatActivity {
+public class StatisticsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
